4834d614fbf1f2890c7794e32fa79fb8cd2e7bf4
balaji <srbalaji435@gmail.com>
2023-06-08 19:57:53 +0530

    First commit

---
+++ ATM-Simulator-System/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,18 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            // Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/bus","root","Balaji@21042003");    
+            s =c.createStatement(); 
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
